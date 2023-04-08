1d54409f7455859850e94421d5fdaa4f1a048010
Mouli VJ <75278840+mouli0007@users.noreply.github.com>
2023-04-08 21:18:32 +0530

    Update Component_Basics.js

--- Component_Basics.js
+++ Component_Basics.js
@@ -2,12 +2,8 @@
 
    npx create-react-app --templeate typescript
    
-   
-   
    ########### Basics Checking of PROPS ##########3
-   
-   
-   
+  
    import React, { ReactElement } from "react";
 
      type HeadingProps = { 
@@ -38,3 +34,60 @@
       };
 
      export default Section;
+
+
+################ TypeScript with Hooks #################
+
+####### 1=> UseState 
+
+import React, { ReactNode } from "react";
+import { useState } from "react";
+
+type CounterProps = {
+  setCount: React.Dispatch<React.SetStateAction<number>>;
+  children: ReactNode;
+};
+const Counter = ({ setCount, children }: CounterProps) => {
+  return (
+    <>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <button onClick={() => setCount((c) => c - 1)}>Decrement</button>
+      <br />
+      <br />
+      {children}
+    </>
+  );
+};
+
+export default Counter;
+
+
+####### TypeScript a Simple Way to Render a List Items ###################
+
+import React from "react";
+
+interface ListProps<T> {
+  items: T[];
+  render: (item: T) => ReactNode;
+}
+
+const List = <T extends {}>({ items, render }: ListProps<T>) => {
+  return (
+    <>
+      <ul>
+        {items.map((item, i) => {
+          return (
+            <>
+              <li key={i}>{render(item)}</li>
+            </>
+          );
+        })}
+      </ul>
+    </>
+  );
+};
+
+####       <List items={["coffee", "Milk", "TEA", "CODE"]} render={render} />
+
+
+export default List;
